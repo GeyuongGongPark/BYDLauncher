5c37d2a22855e47b34b9b15343e396d2a72ffdba
GeyuongGongPark <kar_asirian@naver.com>
2026-08-25 14:31:30 +0900

    chore: app/build/ gitignore 추가

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_bydlauncher_LauncherApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.bydlauncher.LauncherApplication"
-)
-public final class _com_bydlauncher_LauncherApplication {
-}
